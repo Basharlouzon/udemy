f47da5254531e64afd4484fb3518b86385907c5e
Bashar Louzon <bashar.louzon@gmail.com>
2021-07-01 20:32:12 +0300

    debug delete

--- lib/messegner_screen.dart
+++ lib/messegner_screen.dart
@@ -11,15 +11,13 @@ class messengerScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0.0,
+        titleSpacing: 12.0,
         title: Row(
           children: [
             CircleAvatar(
               radius: 20.0,
               backgroundImage: NetworkImage(
                   'https://avatars.githubusercontent.com/u/6737485?v=4'),
-            ),
-            SizedBox(
-              width: 12.0,
             ),
             Text(
               ' Chats',
@@ -33,14 +31,343 @@ class messengerScreen extends StatelessWidget {
         ),
         actions: [
           IconButton(
-            icon: Icon(
-              Icons.camera,
-              size: 20.0,
-              color: Colors.red,
+            icon: CircleAvatar(
+              radius: 20.0,
+              backgroundColor: Colors.black12,
+              child: Icon(
+                Icons.camera_alt,
+                size: 16.0,
+                color: Colors.black,
+              ),
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: CircleAvatar(
+              radius: 20.0,
+              backgroundColor: Colors.black12,
+              child: Icon(
+                Icons.message,
+                size: 16.0,
+                color: Colors.black,
+              ),
             ),
             onPressed: () {},
           ),
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.black12),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.search,
+                    ),
+                    SizedBox(width: .0),
+                    Text('Search',style: TextStyle(color: Colors.black45),),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 12.0),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Container(
+                    width: 60.0,
+                    child: Column(
+                      children: [
+                        Stack(
+                          alignment: AlignmentDirectional.bottomEnd,
+                          children: [
+                            CircleAvatar(
+                              radius: 30.0,
+                              backgroundImage: NetworkImage(
+                                  'https://avatars.githubusercontent.com/u/34492145?v=4'),
+                            ),
+                            Padding(
+                              padding: const EdgeInsetsDirectional.only(
+                                bottom: 2.0,
+                                end: 2.0,
+                              ),
+                              child: CircleAvatar(
+                                radius: 8.0,
+                                backgroundColor: Colors.green,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 8.0),
+                        Text(
+                          'Bashar Louzon',
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 8.0,),
+                  Container(
+                    width: 60.0,
+                    child: Column(
+                      children: [
+                        Stack(
+                          alignment: AlignmentDirectional.bottomEnd,
+                          children: [
+                            CircleAvatar(
+                              radius: 30.0,
+                              backgroundImage: NetworkImage(
+                                  'https://avatars.githubusercontent.com/u/34492145?v=4'),
+                            ),
+                            Padding(
+                              padding: const EdgeInsetsDirectional.only(
+                                bottom: 2.0,
+                                end: 2.0,
+                              ),
+                              child: CircleAvatar(
+                                radius: 8.0,
+                                backgroundColor: Colors.green,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 8.0),
+                        Text(
+                          'Bashar Louzon',
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 8.0,),
+                  Container(
+                    width: 60.0,
+                    child: Column(
+                      children: [
+                        Stack(
+                          alignment: AlignmentDirectional.bottomEnd,
+                          children: [
+                            CircleAvatar(
+                              radius: 30.0,
+                              backgroundImage: NetworkImage(
+                                  'https://avatars.githubusercontent.com/u/34492145?v=4'),
+                            ),
+                            Padding(
+                              padding: const EdgeInsetsDirectional.only(
+                                bottom: 2.0,
+                                end: 2.0,
+                              ),
+                              child: CircleAvatar(
+                                radius: 8.0,
+                                backgroundColor: Colors.green,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 8.0),
+                        Text(
+                          'Bashar Louzon',
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 8.0,),
+                  Container(
+                    width: 60.0,
+                    child: Column(
+                      children: [
+                        Stack(
+                          alignment: AlignmentDirectional.bottomEnd,
+                          children: [
+                            CircleAvatar(
+                              radius: 30.0,
+                              backgroundImage: NetworkImage(
+                                  'https://avatars.githubusercontent.com/u/34492145?v=4'),
+                            ),
+                            Padding(
+                              padding: const EdgeInsetsDirectional.only(
+                                bottom: 2.0,
+                                end: 2.0,
+                              ),
+                              child: CircleAvatar(
+                                radius: 8.0,
+                                backgroundColor: Colors.green,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 8.0),
+                        Text(
+                          'Bashar Louzon',
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),SizedBox(width: 8.0,),
+                  Container(
+                    width: 60.0,
+                    child: Column(
+                      children: [
+                        Stack(
+                          alignment: AlignmentDirectional.bottomEnd,
+                          children: [
+                            CircleAvatar(
+                              radius: 30.0,
+                              backgroundImage: NetworkImage(
+                                  'https://avatars.githubusercontent.com/u/34492145?v=4'),
+                            ),
+                            Padding(
+                              padding: const EdgeInsetsDirectional.only(
+                                bottom: 2.0,
+                                end: 2.0,
+                              ),
+                              child: CircleAvatar(
+                                radius: 8.0,
+                                backgroundColor: Colors.green,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 8.0),
+                        Text(
+                          'Bashar Louzon',
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),SizedBox(width: 8.0,),
+                  Container(
+                    width: 60.0,
+                    child: Column(
+                      children: [
+                        Stack(
+                          alignment: AlignmentDirectional.bottomEnd,
+                          children: [
+                            CircleAvatar(
+                              radius: 30.0,
+                              backgroundImage: NetworkImage(
+                                  'https://avatars.githubusercontent.com/u/34492145?v=4'),
+                            ),
+                            Padding(
+                              padding: const EdgeInsetsDirectional.only(
+                                bottom: 2.0,
+                                end: 2.0,
+                              ),
+                              child: CircleAvatar(
+                                radius: 8.0,
+                                backgroundColor: Colors.green,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 8.0),
+                        Text(
+                          'Bashar Louzon',
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 8.0,),
+                  Container(
+                    width: 60.0,
+                    child: Column(
+                      children: [
+                        Stack(
+                          alignment: AlignmentDirectional.bottomEnd,
+                          children: [
+                            CircleAvatar(
+                              radius: 30.0,
+                              backgroundImage: NetworkImage(
+                                  'https://avatars.githubusercontent.com/u/34492145?v=4'),
+                            ),
+                            Padding(
+                              padding: const EdgeInsetsDirectional.only(
+                                bottom: 2.0,
+                                end: 2.0,
+                              ),
+                              child: CircleAvatar(
+                                radius: 8.0,
+                                backgroundColor: Colors.green,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 8.0),
+                        Text(
+                          'Bashar Louzon',
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              children: [
+                CircleAvatar(
+                  radius: 30.0,
+                  backgroundImage: NetworkImage(
+                      'https://avatars.githubusercontent.com/u/34492145?v=4'),
+                ),
+              ],
+            ),
+
+          ],
+        ),
       ),
     );
   }
